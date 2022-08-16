5e3868832d7cfbdc81b8bdd296166544cad4b60f
G1e6 <gleb-potapov@rambler.ru>
2022-08-16 21:02:34 +0500

    ferst commit

---
+++ src/test/java/ru/netology/PostmanEcho/PostmatEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology.PostmanEcho;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmatEchoTest {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
+        ;
+
+
+    }
+}
